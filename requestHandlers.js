d71014688ed0b20fe85f56f25e8121b60eb13316
robertlyc <merlinlyc@gmail.com>
2012-08-08 13:56:53 +0800

    add unblock to handlers

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,6 +1,24 @@
+var exec = require("child_process").exec;
+
 function start () {
 	console.log("Request hanlder 'start' was called.");
-	return "Hello Strat";
+	
+	var content = "empty";
+	
+	exec("ls -lash", function (error, stdout, stderr) {
+		content = stdout;
+	});
+	
+	return content;
+	
+	// //add sleep function
+	// 	function sleep (milliSeconds) {
+	// 		var startTime = new Date().getTime();
+	// 		while (new Date().getTime() < startTime + milliSeconds);
+	// 	}
+	// 	
+	// 	sleep(10000);
+	// 	return "Hello Strat";
 }
 
 function upload () {
