ef462956e2cde21df27c97a9b1d009517318af32
robertlyc <merlinlyc@gmail.com>
2012-08-08 12:44:12 +0800

    add response code in several js files

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,11 @@
 function start () {
 	console.log("Request hanlder 'start' was called.");
+	return "Hello Strat";
 }
 
 function upload () {
 	console.log("Request handler 'upload' was called.");
+	return "Hello Upload";
 }
 
 exports.start = start;
