f44a7d1f0344e5b36d20c07b62ce5b6c8abe3b07
robertlyc <merlinlyc@gmail.com>
2012-08-11 21:12:15 +0800

    增加图片选择框

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,5 @@
-var querystring = require('querystring');
-var fs = require('fs');
+var querystring = require("querystring");
+var fs = require("fs");
 
 function start (response, postData) {
 	console.log("Request hanlder 'start' was called.");
@@ -10,9 +10,10 @@ function start (response, postData) {
 	  'charset=UTF-8" />'+
 	  '</head>'+
 	  '<body>'+
-	  '<form action="/upload" method="post">'+
-	  '<textarea name="text" rows="20" cols="60"></textarea>'+
-	  '<input type="submit" value="Submit text" />'+
+	  '<form action="/upload" enctype="multipart/form-data"'+
+	  'method="post"'+
+	  '<input type="file" name="upload">'+
+	  '<input type="submit" value="Upload file" />'+
 	  '</form>'+
 		'</body>'+
 		'</html>';
