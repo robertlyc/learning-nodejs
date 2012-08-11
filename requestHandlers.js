9a6b55768a896b051165b3c50966838a2df97ef7
robertlyc <merlinlyc@gmail.com>
2012-08-11 15:45:11 +0800

    重构response模块

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,29 +1,20 @@
 var exec = require("child_process").exec;
 
-function start () {
+function start (response) {
 	console.log("Request hanlder 'start' was called.");
 	
-	var content = "empty";
-	
 	exec("ls -lah", function (error, stdout, stderr) {
-		content = stdout;
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.write(stdout);
+		response.end();
 	});
-	
-	return content;
-	
-	// //add sleep function
-	// 	function sleep (milliSeconds) {
-	// 		var startTime = new Date().getTime();
-	// 		while (new Date().getTime() < startTime + milliSeconds);
-	// 	}
-	// 	
-	// 	sleep(10000);
-	// 	return "Hello Strat";
 }
 
-function upload () {
+function upload (response) {
 	console.log("Request handler 'upload' was called.");
-	return "Hello Upload";
+	response.writeHead(200, {"Content-Type": "text/plain"});
+	response.write("Hello Upload");
+	response.end();
 }
 
 exports.start = start;
