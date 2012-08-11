ecfed0a3fe9e3542894b4ac3ac0196074f319d46
robertlyc <merlinlyc@gmail.com>
2012-08-11 22:16:50 +0800

    增加图片上传

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 var querystring = require("querystring");
 var fs = require("fs");
+var formidable = require("formidable");
 
-function start (response, postData) {
+function start (response) {
 	console.log("Request hanlder 'start' was called.");
 	
 	var body = '<html>'+
@@ -12,25 +13,38 @@ function start (response, postData) {
 	  '<body>'+
 	  '<form action="/upload" enctype="multipart/form-data"'+
 	  'method="post"'+
-	  '<input type="file" name="upload">'+
+	  '<input type="file" name="upload" multiple="multiple">'+
 	  '<input type="submit" value="Upload file" />'+
 	  '</form>'+
-		'</body>'+
-		'</html>';
+	  '</body>'+
+	  '</html>';
 		
-		response.writeHead(200, {"Content-Type": "text/html"});
-		response.write(body);
-		response.end();
-}
-
-function upload (response,postData) {
-	console.log("Request handler 'upload' was called.");
-	response.writeHead(200, {"Content-Type": "text/plain"});
-	response.write("You've sent " + querystring.parse(postData).text);
+	response.writeHead(200, {"Content-Type": "text/html"});
+	response.write(body);
 	response.end();
 }
 
-function show (response, postData) {
+function upload (response,request) {
+	console.log("Request handler 'upload' was called.");
+	
+	var	form = new formidable.IncomingForm();
+	console.log("about to parse");
+	form.parse(request, function(error, fields, files) {
+		console.log("about to parse");
+    fs.rename(files.upload.path, "/tmp/test.png", function(err) {
+      if (err) {
+        fs.unlink("/tmp/test.png");
+        fs.rename(files.upload.path, "/tmp/test.png");
+      }
+    });
+		response.writeHead(200, {"Content-Type": "text/html"});
+		response.write("received image:<br/>");
+		response.write("<img src='/show' />");
+		response.end();
+	});
+}
+
+function show (response) {
 	console.log("Request handler 'show' was called.");
 	fs.readFile("/tmp/test.png", "binary", function(error, file) {
 		if (error) {
